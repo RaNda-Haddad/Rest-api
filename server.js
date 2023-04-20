71b6577e65aa4c8c0a9323768a9bd14c6a59992b
RaNdaHaddad <randahaddad8@gmail.com>
2023-04-20 14:28:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+
+const express=require ('express')
+const app=express()
+const search=require("./config/connectDB")
+
+require("dotenv").config({ path: "./config/.env" });
+// 1//creat server
+   //*** 1.1 run server 
+   //*** 1.2 conntDB (config folder)
+   //*** 1.3 creat Route
+   //*** 1.4 parse Data
+
+   
+//*** 1.2-connect db
+search.connectDB(); 
+
+//3-routes
+app.use(express.json())
+//Create and save User function
+
+search.createSave()
+//Create Many Records with model.create()
+search.createPeoples({name:"ahmed",age:20,favoriteFoods:["brik","keftegi"]},)
+
+//Find User ByName
+
+search.findByName('ahmed')
+//Find User By ID function
+
+search.findById('60e70c7231bbab1b0cab06fe')
+//Find User By Food
+search.findByFood('brik')
+
+//Perform Classic Updates by Running Find, Edit, then Save
+//search.findEdit('60e70c7231bbab1b0cab06fe')
+//Update User
+search.findAndUpdate('ahmed')
+//Delete User
+
+search.removeById('60e70c7231bbab1b0cab06fe')
+
+//Delete All User
+
+search.removePeoples('brik')
+
+//Get Specific user
+
+//search.queryChain()
+
+
+
+
+
+//*** 1.1 run
+const PORT = process.env.PORT || process.env.port;
+app.listen(PORT, err => {
+    err
+        ? console.log(err)
+        : console.log(`the server is running on http://localhost:${PORT}`)
+})
